ff447ab3bea3472ab32e58ef3349f2d7e0e71f77
Mehdi Cherti <m.cherti@fz-juelich.de>
2024-03-27 19:23:18 +0100

    also add support to winoground

--- clip_benchmark/metrics/generative_image_caption_selection.py
+++ clip_benchmark/metrics/generative_image_caption_selection.py
@@ -18,9 +18,14 @@ def score_aligned(logits, texts):
     ce = lp.sum(dim=(1,2))
     return ce
 
+
 def evaluate(model, dataloader, tokenizer,  device, amp=True):
     """
-    Evaluate the model on the given dataset
+    Evaluate the model on the given dataset.
+    The task has N instances, each instance has I images and C captions.
+    For each instance, the goal is to find the correct image for each caption and the correct caption for each image.
+    This is done by computing the similarities between each image and each caption.
+    This procedure is used to evaluate the models on Winoground and SugarCrepe.
 
     Parameters
     ----------
@@ -41,37 +46,44 @@ def evaluate(model, dataloader, tokenizer,  device, amp=True):
     Returns
     -------
     
-    dict of accuracy metric
+    dict of accuracy metrics
     """
-    amp = False
     autocast = torch.cuda.amp.autocast if amp else suppress
-    preds = []
+    image_score = []
+    text_score = []
+    score = []
     for batch_images, batch_texts in tqdm(dataloader):
+        if len(batch_images.shape) == 4:
+            B, C, H, W = batch_images.shape
+            batch_images = batch_images.view(B, 1, C, H, W)
+        # batch_images: B, nb_images_per_instance, C, H, W
+        # batch_texts: B, nb_captions_per_instance
+        
+        B, nim, C, H, W = batch_images.shape
+        nt = len(batch_texts[0])
         batch_images = batch_images.to(device)
+        batch_images_ = batch_images.view(B*nim, C, H, W) # B*nim, C, H, W
         # tokenize all texts in the batch
-        batch_texts_tok = tokenizer([text for i, texts in enumerate(batch_texts) for text in texts]).to(device)
-        batch_texts_raw = ([text for i, texts in enumerate(batch_texts) for text in texts])#.to(device)
-
-        nb_texts_for_each_image = [len(texts) for texts in batch_texts]
-
+        batch_texts_tok_ = tokenizer([text for i, texts in enumerate(batch_texts) for text in texts]).to(device)
+        batch_texts_tok_ = batch_texts_tok_.view(B, nt, -1)
         # compute the embedding of images and texts
         with torch.no_grad(), autocast():
-            C, H, W = batch_images.shape[1:]# TODO - make this a parameter
-
-            _, batch_images_emb = model._encode_image(batch_images)
-            start = 0
-            for i, nb in enumerate(nb_texts_for_each_image):
-                end = start + nb
-                image_emb = batch_images_emb[i:i+1]
-                texts = batch_texts_tok[start:end]
-                max_text_len = (texts==0).float().argmax(dim=-1).max().item()
-                lens = (texts==0).float().argmax(dim=-1)
-                texts = texts[:, :max_text_len]
-                if torch.any(torch.isnan(image_emb)):
-                    print("Detected nans in image embs..")
-                    return {"acc": 0.0}
-                nim, lim, dim = image_emb.shape
-                ntext, ltext = texts.shape
+            _, batch_images_emb = model._encode_image(batch_images_)
+            bs, seqlen, hidden = batch_images_emb.shape
+            batch_images_emb = batch_images_emb.view(B, nim, seqlen, hidden)
+            gt = torch.arange(min(nim, nt)).to(device)
+        for i in range(B):
+            # iteratve over instances
+            image_emb = batch_images_emb[i]
+            texts = batch_texts_tok_[i]
+            max_text_len = (texts==0).float().argmax(dim=-1).max().item()
+            texts = texts[:, :max_text_len]
+            if torch.any(torch.isnan(image_emb)):
+                print("Detected nans in image embs..")
+                return {"acc": 0.0}
+            nim, lim, dim = image_emb.shape
+            ntext, ltext = texts.shape
+            with torch.no_grad(), autocast():
                 image_embs_p = image_emb.view(nim, 1, lim, dim).repeat(1, ntext, 1, 1).view(nim*ntext, lim, dim)
                 texts_p = texts.view(1, ntext, ltext).repeat(nim, 1, 1).view(nim*ntext, ltext)
                 
@@ -81,19 +93,18 @@ def evaluate(model, dataloader, tokenizer,  device, amp=True):
                 logits = model.text_decoder(image_embs_p, input_text_embs_p)
                 scores = score_aligned(logits, out_text)
                 scores = scores.view(nim, ntext)
-                scores = scores[0]
-                if torch.any(torch.isnan(scores)):
-                    print("Detected nans..")
-                    return {"acc": 0.0}
-                pred = scores.argmax().item()
-                value = scores[0].item()
-                all_equal = torch.all(scores==value).item()
-                if all_equal:
-                    # tie
-                    pred = -1
-                start = end 
-                preds.append(pred)
-    pred = torch.Tensor(preds).long()
-    acc = (pred==0).float().mean().item() 
-    metrics = {"acc": acc}
+
+                # i-th image should be matched to the i-th text
+                image_closest_text = scores.argmax(dim=1)[:len(gt)]
+                text_closest_image = scores.argmax(dim=0)[:len(gt)]
+                pred_text_is_correct = (image_closest_text==gt).all().item()
+                pred_image_is_correct = (text_closest_image==gt).all().item()
+            all_correct = pred_text_is_correct and pred_image_is_correct
+            image_score.append(pred_image_is_correct)
+            text_score.append(pred_text_is_correct)
+            score.append(all_correct)
+    metrics = {}
+    metrics["image_acc"] = torch.Tensor(image_score).float().mean().item()
+    metrics["text_acc"] = torch.Tensor(text_score).float().mean().item()
+    metrics["acc"] = torch.Tensor(score).float().mean().item()
     return metrics
