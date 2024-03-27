b6316ca16e6444095534843101b601360ead3c23
Mehdi Cherti <m.cherti@fz-juelich.de>
2024-03-27 18:21:40 +0100

    fix tie problem

--- clip_benchmark/metrics/generative_image_caption_selection.py
+++ clip_benchmark/metrics/generative_image_caption_selection.py
@@ -65,6 +65,7 @@ def evaluate(model, dataloader, tokenizer,  device, amp=True):
                 image_emb = batch_images_emb[i:i+1]
                 texts = batch_texts_tok[start:end]
                 max_text_len = (texts==0).float().argmax(dim=-1).max().item()
+                lens = (texts==0).float().argmax(dim=-1)
                 texts = texts[:, :max_text_len]
                 if torch.any(torch.isnan(image_emb)):
                     print("Detected nans in image embs..")
@@ -79,13 +80,17 @@ def evaluate(model, dataloader, tokenizer,  device, amp=True):
                 _, input_text_embs_p = model._encode_text(input_text)
                 logits = model.text_decoder(image_embs_p, input_text_embs_p)
                 scores = score_aligned(logits, out_text)
-
                 scores = scores.view(nim, ntext)
                 scores = scores[0]
                 if torch.any(torch.isnan(scores)):
                     print("Detected nans..")
                     return {"acc": 0.0}
                 pred = scores.argmax().item()
+                value = scores[0].item()
+                all_equal = torch.all(scores==value).item()
+                if all_equal:
+                    # tie
+                    pred = -1
                 start = end 
                 preds.append(pred)
     pred = torch.Tensor(preds).long()
